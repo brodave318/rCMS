e283b3e03a6e6557cf5bf22b5354b1e45863c7d1
David <brodave318@gmail.com>
2017-05-07 19:20:26 -0700

    Customized dashboard | Implemented html editor | Added actions to Blocks index

--- app/admin/block.rb
+++ app/admin/block.rb
@@ -19,13 +19,14 @@ ActiveAdmin.register Block do
     column :display, :sortable => :display
     column :created_at, :sortable => :created_at
     column :order
+    actions
   end
 
   form do |f|
     f.inputs "Details" do
       f.input :title, :label => "Title"
       f.input :show_title, :label => "Show Title"
-      f.input :body, :label => "Body"
+      f.input :body, as: :html_editor, :label => "Body"
       f.input :position, :label => "Position", :as => :select, :collection => [["Jumbotron", "jumbotron"], ["Block", "block"]]
       f.input :display, :label => "Display", :as => :select, :collection => [["All pages", "all"], ["Homepage Only", "home"], ["All But Homepage", "nohome"]]
       f.input :order, :label => "Order"
--- app/admin/page.rb
+++ app/admin/page.rb
@@ -25,7 +25,7 @@ permit_params :title, :body, :order, :is_published, :section_id, :menu_display
     f.inputs "Details" do
       f.input :title, :label => "Title"
       f.input :section, :label => "Section"
-      f.input :body, :label => "Body"
+      f.input :body, as: :html_editor, :label => "Body"
       f.input :order, :label => "Order"
       f.input :is_published, :label => "Published"
       f.input :menu_display, :label => "Display In Menu"
