c5c2597bd8de9cd38085bfc7be92de456f2cdb4a
David <brodave318@gmail.com>
2017-05-08 17:10:49 -0700

    Added  syntax to show pages | Created partial files for Blocks | Block Show page finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_filter :getPageNav
+  before_filter :getPageNav, :getJumbotron, :getBlocks
   protect_from_forgery with: :exception
 
   def getPageNav
     @pageNav = Page.where("menu_display = true && is_published = true").order(order: :asc)
+  end
+
+  def getJumbotron
+    @jumbotron = Block.where("position = 'jumbotron'")
+  end
+
+  def getBlocks
+    @blocks = Block.where("position = 'block'").order(order: :asc)
   end
 end
